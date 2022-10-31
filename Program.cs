d3227765023a4361031f889bea8769f0dcb3600c
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-31 12:51:38 +0000

    It works!

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿string sourceDirectory = "D:\\AUTOMATION TESTING DIRECTORY\\ImageMetadata\\TBS";
-string baseTargetDirectory = "D:\\AUTOMATION TESTING DIRECTORY\\ImageMetadata\\RAW";
+﻿string sourceDirectory = "E:\\TBS\\unsorted\\";
+string baseTargetDirectory = "E:\\TBS\\sorted\\";
 
 int count = 0;
+int total = Directory.EnumerateFiles(sourceDirectory).Where(File => File.EndsWith(".JPG") || File.EndsWith(".CR3") || File.EndsWith(".MP4")).Count();
 
-foreach (string x in Directory.EnumerateFiles(sourceDirectory).Where(File => File.EndsWith(".jpg|.cr3")))
+foreach (string x in Directory.EnumerateFiles(sourceDirectory).Where(File => File.EndsWith(".JPG") || File.EndsWith(".CR3") || File.EndsWith(".MP4")))
 {
     count++;
 
@@ -18,9 +19,15 @@ foreach (string x in Directory.EnumerateFiles(sourceDirectory).Where(File => Fil
 
     string path = Path.Combine(baseTargetDirectory, year.ToString(), month.ToString(), day.ToString(), Path.GetFileName(x));
 
+
+    if (!Directory.Exists(Path.GetDirectoryName(path)))
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(path));
+    }
+    
     File.Move(x, path);
 
-    Console.WriteLine(count + ": File moved to: " + path);
+    Console.WriteLine(count + " of " + total + " files sorted");
 
     Console.WriteLine("===============================================");
 }
