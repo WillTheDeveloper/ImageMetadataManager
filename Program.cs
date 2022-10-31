92834dacc885e900afc9130f27274ec2755c365d
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-31 12:59:21 +0000

    Added notes to program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿string sourceDirectory = "E:\\TBS\\unsorted\\";
-string baseTargetDirectory = "E:\\TBS\\sorted\\";
+﻿string sourceDirectory = "E:\\TBS\\unsorted\\"; // Location of unsorted files
+string baseTargetDirectory = "E:\\TBS\\sorted\\"; // The location where you want sorted files
 
 int count = 0;
-int total = Directory.EnumerateFiles(sourceDirectory).Where(File => File.EndsWith(".JPG") || File.EndsWith(".CR3") || File.EndsWith(".MP4")).Count();
+int total = Directory.EnumerateFiles(sourceDirectory).Where(File => File.EndsWith(".JPG") || File.EndsWith(".CR3") || File.EndsWith(".MP4")).Count(); // Total count of files to be moved
 
 foreach (string x in Directory.EnumerateFiles(sourceDirectory).Where(File => File.EndsWith(".JPG") || File.EndsWith(".CR3") || File.EndsWith(".MP4")))
 {
@@ -17,15 +17,15 @@ foreach (string x in Directory.EnumerateFiles(sourceDirectory).Where(File => Fil
     Console.WriteLine(month);
     Console.WriteLine(year);
 
-    string path = Path.Combine(baseTargetDirectory, year.ToString(), month.ToString(), day.ToString(), Path.GetFileName(x));
+    string path = Path.Combine(baseTargetDirectory, year.ToString(), month.ToString(), day.ToString(), Path.GetFileName(x)); // Construct the path
 
 
-    if (!Directory.Exists(Path.GetDirectoryName(path)))
+    if (!Directory.Exists(Path.GetDirectoryName(path))) // If the path does not currently exist then create the directory
     {
         Directory.CreateDirectory(Path.GetDirectoryName(path));
     }
     
-    File.Move(x, path);
+    File.Move(x, path); // Move the file to the new location
 
     Console.WriteLine(count + " of " + total + " files sorted");
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ImageMetadataManager
+# Image Metadata Manager
+
